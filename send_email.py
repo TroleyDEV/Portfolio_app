08884931b2a668a92b971024bb670f75906c47c9
Troley <troleyhs@gmail.com>
2024-03-07 18:28:54 +0100

    Implement Enviromental Variable to protect my credentials -  Project Finished

--- send_email.py
+++ send_email.py
@@ -1,5 +1,6 @@
 import smtplib
 import ssl
+import os
 
 
 def send_email(message, receiver):
@@ -7,7 +8,8 @@ def send_email(message, receiver):
     port = 465
 
     username = "mateusz.mlynarczyk82@gmail.com"
-    password = "enqe qkop quvg jzkd"
+    password = os.getenv("PASSWORD_PORTFOLIO_APP")
+    print(password)
 
     context = ssl.create_default_context()
 
